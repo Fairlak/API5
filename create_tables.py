ae97dcd9de073a50879fc8b4ac6386d0f202dda9
Fairlak <Azarkevich.2005@mail.ru>
2023-02-12 14:24:20 +0300

    Changed names and moved print

--- create_tables.py
+++ create_tables.py
@@ -10,20 +10,20 @@ from headhunter import get_statistics_languages_hh
 
 
 def create_table(statistics_languages, title):
-    table = [
+    table_header = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
         ]
     for language, vacancy in statistics_languages.items():
-        table_data = [
+        table_content = [
             language,
             vacancy['vacancies_found'],
             vacancy['vacancies_processed'],
             vacancy['average_salary']
         ]
-        table.append(table_data)
-        table_instance = AsciiTable(table, title)
+        table_header.append(table_content)
+        table_instance = AsciiTable(table_header, title)
         table_instance.justify_columns[2] = 'left'
-    print(table_instance.table)
+    return table_instance.table
 
 
 def main():
@@ -40,9 +40,9 @@ def main():
         'Go'
     ]
     sj_languages_statistics = get_statistics_languages_sj(programming_languages, superjob_key)
-    create_table(sj_languages_statistics, title='SuperJob Moscow')
+    print(create_table(sj_languages_statistics, title='SuperJob Moscow'))
     hh_languages_statistics = get_statistics_languages_hh(programming_languages)
-    create_table(hh_languages_statistics, title='Headhunter Moscow')
+    print(create_table(hh_languages_statistics, title='Headhunter Moscow'))
 
 
 
